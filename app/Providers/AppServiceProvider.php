16bc82c5a4221142f9ad5b8c57a4edbc311d4ffe
BVAk <bekkviktoria55@gmail.com>
2020-04-13 09:33:32 +0300

    Services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use App\Articles\ArticlesRepository;
+use App\Articles\ElasticsearchArticlesRepository;
 use App\Articles\EloquentArticlesRepository;
 use Illuminate\Support\ServiceProvider;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,13 +16,30 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-     public function register()
-        {
-            $this->app->bind(ArticlesRepository::class, function () {
+    public function register()
+    {
+        $this->app->singleton(ArticlesRepository::class, function($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (!config('services.search.enabled')) {
                 return new EloquentArticlesRepository();
-            });
-        }
-    
+            }
+
+            return new ElasticsearchArticlesRepository(
+                $app->make(Client::class)
+            );
+        });
+    }
+
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts(config('services.search.hosts'))
+                ->build();
+        });
+    }
 
     /**
      * Bootstrap any application services.
